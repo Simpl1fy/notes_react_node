de8ffbb1f4248736622a65cf08b172c4e2bfa4f2
Simpl1fy <gourabdas07012004@gmail.com>
2024-08-23 20:51:26 +0530

    added code for /login

--- notes-backend/routes/userRoutes.js
+++ notes-backend/routes/userRoutes.js
@@ -38,4 +38,32 @@ router.post('/signup', async(req, res) => {
     }
 })
 
+router.post('/login', async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(102).json({"Error": "Please fill out all the information"})
+        }
+        const [rows] = await conn.query('select * from users where email = ?', [email]);
+        // console.log(rows[0].password);
+        if (rows.length === 0) {
+            return res.status(300).json({"Error": "Please sign up first!"});
+        }
+        const hashedPassword = rows[0].password;
+        console.log(password);
+        console.log(hashedPassword);
+        const bool = await bcrypt.compare(password, hashedPassword);
+        console.log(bool);
+        if(bool) {
+            console.log("You have been logged in succesfully");
+            return res.status(200).json({"Message": "You have been logged in succesfully!"});
+        } else {
+            return res.status(300).json({"Message": "Password does not match"});
+        }
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({"Error": "Internal Server Error!"});
+    }
+})
+
 module.exports = router;
