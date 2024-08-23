9330fdd8ea27b2eaf2b39ffe4b40745c938ae078
Simpl1fy <gourabdas07012004@gmail.com>
2024-08-23 21:13:29 +0530

    added changes to status codes

--- notes-backend/routes/userRoutes.js
+++ notes-backend/routes/userRoutes.js
@@ -4,54 +4,64 @@ const bcrypt = require('bcrypt');
 
 const conn = require('./../connection');
 
+const saltRounds=10;
+
 router.post('/signup', async(req, res) => {
     try {
         const { name, email, password } = req.body;
-        const saltRounds=10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
         
         // validate input
         if(!name || !email || !password) {
             return res.status(102).json({"Error": "Please fill out all the information"})
         }
-
+        
         // const checkUserAlreadyExists = 'select * from users where email = ?';
         const [rows] = await conn.query('select * from users where email = ?', [email]);
         if (rows.length > 0) {
-            res.status(409).json({"Error": "User Already Exists"});
+            return res.status(409).json({"Error": "User Already Exists"});
         }
-
+        // hash the password
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        // insert the data into the database
         await conn.query('insert into users (name, email, password) values (?,?,?)', [name, email, hashedPassword]);
-
-        res.status(201).json({"Message": "User has been created"});
+        
+        // return response
+        return res.status(201).json({"Message": "User has been created"});
     } catch(err) {
         console.error(err);
-        res.status(500).json({Error: "Internal Server Error!"});
+        return res.status(500).json({Error: "Internal Server Error!"});
     }
 })
 
 router.post('/login', async(req, res) => {
     try {
         const { email, password } = req.body;
+        
+        // checking for input
         if(!email || !password) {
-            return res.status(102).json({"Error": "Please fill out all the information"})
+            return res.status(400).json({"Error": "Please fill out all the information"})
         }
+
+        // checking if the email exists or not
         const [rows] = await conn.query('select * from users where email = ?', [email]);
         // console.log(rows[0].password);
         if (rows.length === 0) {
-            return res.status(300).json({"Error": "Please sign up first!"});
+            return res.status(404).json({"Error": "Please sign up first!"});
         }
+
+        // hashed password
         const hashedPassword = rows[0].password;
-        console.log(password);
-        console.log(hashedPassword);
-        const bool = await bcrypt.compare(password, hashedPassword);
-        console.log(bool);
-        if(bool) {
-            console.log("You have been logged in succesfully");
-            return res.status(200).json({"Message": "You have been logged in succesfully!"});
-        } else {
-            return res.status(300).json({"Message": "Password does not match"});
+        console.log(`Input Password = ${password}`);
+        console.log(`Hashed Password stored in db = ${hashedPassword}`);
+        // comparing the input password with the hashed password
+        const isMatch = await bcrypt.compare(password, hashedPassword);
+        console.log(isMatch);
+
+        // false condition
+        if(!isMatch) {
+            return res.status(401).json({"Error": "Password does not match!"});
         }
+        return res.status(200).json({"message": "You have been logged in!"});
     } catch(err) {
         console.error(err);
         res.status(500).json({"Error": "Internal Server Error!"});
