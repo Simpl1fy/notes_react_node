36fd992eb995473fd7dc22481de0c8d15e4a38a2
Simpl1fy <gourabdas07012004@gmail.com>
2024-08-23 21:43:11 +0530

    added cors to backend

--- notes-backend/server.js
+++ notes-backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const conn = require('./connection');
 
 
 const app = express();
+
+app.use(cors());
 
 // Middleware
 // parse application/x-www-form-urlencoded
