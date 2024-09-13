af33c32c70116d3597198cee5edbb03d1536479d
Simpl1fy <gourabdas07012004@gmail.com>
2024-09-13 15:41:26 +0530

    created profile page

--- todo-frontend/src/components/Profile.jsx
+++ todo-frontend/src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useAuth } from "./useAuth"
+import { Button } from "react-bootstrap";
 
 export default function Profile() {
 
@@ -31,13 +32,24 @@ export default function Profile() {
 
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
-      <div className="p-3 bg-light-subtle border">
-        <h3>Your Profile</h3>
-        <div className="container">
+      <div className="p-3 bg-light-subtle border" style={{width: "30rem"}}>
+        <h3 className="mb-2">Your Profile</h3>
+        <div className="container d-flex flex-column">
           {profileData ? (
           <>
-            <div>Name: {profileData[0].name}</div>
-            <div>Email: {profileData[0].email}</div>
+            <div className="mt-2">Name: {profileData[0].name}</div>
+            <div className="d-flex justify-content-between align-items-center mt-2">
+              <div>Email: {profileData[0].email}</div>
+              <div>
+                <Button variant="primary">Update Email</Button>
+              </div>
+            </div>
+            <div className="d-flex justify-content-between align-items-center mt-2">
+              <div>Password</div>
+              <div>
+                <Button variant="primary">Change Password</Button>
+              </div>
+            </div>
           </>
           ) : (
             <>
