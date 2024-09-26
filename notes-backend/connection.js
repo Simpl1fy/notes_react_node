7732e15bd7294817726f004df28ebdd40aeec814
Simpl1fy <gourabdas07012004@gmail.com>
2024-09-26 23:57:08 +0530

    implemented changes for production

--- notes-backend/connection.js
+++ notes-backend/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require("dotenv").config();
 
 const conn = mysql.createPool({
-  host: process.env.LOCAL_DB_HOST,
-  user: process.env.LOCAL_DB_USER,
-  password: process.env.LOCAL_DB_PASSWORD,
-  database: process.env.LOCAL_DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
--- todo-frontend/src/config/axiosConfig.js
+++ todo-frontend/src/config/axiosConfig.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 
 // https://backend-notes-cp19.onrender.com/
+// http://localhost:5000
 
 // Set the base URL for your backend API
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://backend-notes-cp19.onrender.com/",
 });
 
 export default api;
