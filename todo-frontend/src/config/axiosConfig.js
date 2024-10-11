413086f15f85b3fba509a9bcdfdf303adab73c88
Simpl1fy <gourabdas07012004@gmail.com>
2024-10-11 19:44:01 +0530

    changed axios config

--- todo-frontend/src/config/axiosConfig.js
+++ todo-frontend/src/config/axiosConfig.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // Set the base URL for your backend API
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://backend-notes-cp19.onrender.com",
 });
 
 export default api;
